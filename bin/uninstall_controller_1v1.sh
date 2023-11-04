290aef893895787d8ac8db9ab3a262cfaa5fd91a
Jason Russell <grayhatguy@grayhatguy.com>
2023-11-04 16:09:22 -0400

    Update uninstall_controller_1v1.sh

--- bin/uninstall_controller_1v1.sh
+++ bin/uninstall_controller_1v1.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 ## ~/rpi_k3s/bin/uninstall_worker_1v1.sh
 ## uninstall_worker_1v1.sh uninstall k3s k3d and docker from worker
-# k3s nuke
-sudo sh /usr/local/bin/k3s-uninstall.sh
-# k3d nuke
-sudo rm -rf /usr/local/bin/k3d
 # docker nuke power down images prune network volumes verify with ps 
 docker container stop $(docker container ls -aq) ; docker container rm -f $(docker container ls -aq) ; docker rmi -f $(docker images -aq) ; docker volume prune && docker network prune && docker ps
 # purge docker pkg
@@ -13,3 +9,7 @@ sudo dpkg -l | grep -i docker && sudo apt-get purge -y docker-engine docker dock
 sudo rm -rf /var/lib/docker /etc/docker && sudo rm -rf /etc/apparmor.d/docker && sudo groupdel docker && sudo rm -rf /var/run/docker.sock
 # good practice
 sudo apt-get upgrade -y && apt-get update -y 
+# k3d nuke
+sudo rm -rf /usr/local/bin/k3d
+# k3s nuke
+sudo sh /usr/local/bin/k3s-agent-uninstall.sh
