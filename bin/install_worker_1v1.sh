4291b764880d25b60b5627e0063caa980ff489f8
Jason Russell <grayhatguy@grayhatguy.com>
2023-11-04 16:33:56 -0400

    Update install_worker_1v1.sh

--- bin/install_worker_1v1.sh
+++ bin/install_worker_1v1.sh
@@ -9,22 +9,22 @@ sudo apt-get -y install policycoreutils
 # root install shell
 sudo -s
 # docker install
-apt-get update -y && apt-get upgrade -y && curl -fsSL https://get.docker.com -o get-docker.sh && bash get-docker.sh
-# good practice
-sudo apt-get upgrade -y && apt-get update -y 
+apt-get update -y && apt-get upgrade -y && curl -fsSL https://get.docker.com -o get-docker.sh && bash get-docker.sh ; wait
 # docker own
-sudo usermod -aG docker $USER && newgrp docker && sudo chown "$USER":"$USER" /home/"$USER"/.docker -R && chmod g+rwx "$HOME/.docker" -R 
+sudo usermod -aG docker $USER && newgrp docker && sudo chown "$USER":"$USER" /home/"$USER"/.docker -R && chmod g+rwx "$HOME/.docker" -R ; wait
 # docker startup
-sudo systemctl enable docker.service && sudo systemctl enable containerd.service
+sudo systemctl enable docker.service && sudo systemctl enable containerd.service ; wait
+# good practice
+sudo apt-get upgrade -y && apt-get update -y ; wait
 exit
 # k3d install
-sudo wget -O k3d-linux-arm64 https://github.com/rancher/k3d/releases/download/v3.1.5/k3d-linux-arm64 && sudo mv k3d-linux-arm64 /usr/local/bin/k3d && sudo chmod +x /usr/local/bin/k3d
+sudo wget -O k3d-linux-arm64 https://github.com/rancher/k3d/releases/download/v3.1.5/k3d-linux-arm64 && sudo mv k3d-linux-arm64 /usr/local/bin/k3d && sudo chmod +x /usr/local/bin/k3d ; wait
 # Prompt the user for Controller token /var/lib/rancher/k3s/server/node-token and controller URL and install k3s
 echo "Enter controller /var/lib/rancher/k3s/server/node-token: "
 read token
 echo echo "Control token is: $TOKEN"
-read -p "Enter controller token from /var/lib/rancher/k3s/server/node-token: " TOKEN
-read -p "Enter IP address: " IP
+read -p "Enter controller token from /var/lib/rancher/k3s/server/node-token: " TOKEN ; wait
+read -p "Enter IP address: " IP ; wait
 echo "Control ip is: $IP"
 curl -sfL https://get.k3s.io | K3S_URL=https://$IP:6443 K3S_TOKEN=$TOKEN sh - ; wait
 # good practice
