43498a50c52968263a2c39f6f1e02f8127b8f11b
Jason Russell <grayhatguy@grayhatguy.com>
2023-11-04 18:47:48 -0400

    Update uninstall_worker_1v1.sh

--- bin/uninstall_worker_1v1.sh
+++ bin/uninstall_worker_1v1.sh
@@ -25,6 +25,6 @@ sudo sh /usr/local/bin/k3s-agent-uninstall.sh &&
 wait
 echo "k3s nuked"
 # good practice
-sudo apt-get upgrade -y && apt-get update -y &&
+sudo apt-get upgrade -y && sudo apt-get update -y &&
 wait
 echo "Uninstall complete"
