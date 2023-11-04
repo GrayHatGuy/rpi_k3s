16db3429d1d602ec1db452544433aba501c960b6
Jason Russell <grayhatguy@grayhatguy.com>
2023-11-04 16:01:45 -0400

    Update uninstall_controller_1v1.sh

--- bin/uninstall_controller_1v1.sh
+++ bin/uninstall_controller_1v1.sh
@@ -6,7 +6,7 @@ sudo sh /usr/local/bin/k3s-uninstall.sh
 # k3d nuke
 sudo rm -rf /usr/local/bin/k3d
 # docker nuke power down images prune network volumes verify with ps 
-docker container stop $(docker container ls -aq) && docker container rm -f $(docker container ls -aq) && docker rmi -f $(docker images -aq) && docker volume prune && docker network prune && docker ps
+docker container stop $(docker container ls -aq) ; docker container rm -f $(docker container ls -aq) ; docker rmi -f $(docker images -aq) ; docker volume prune && docker network prune && docker ps
 # purge docker pkg
 sudo dpkg -l | grep -i docker && sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin && sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin
 # kill docker lib data groups and sockets
