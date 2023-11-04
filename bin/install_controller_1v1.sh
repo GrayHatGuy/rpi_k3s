d71bba3c779de5e6158fed3b503e91376e29d5bd
Jason Russell <grayhatguy@grayhatguy.com>
2023-11-04 18:08:30 -0400

    Update install_controller_1v1.sh

--- bin/install_controller_1v1.sh
+++ bin/install_controller_1v1.sh
@@ -4,34 +4,34 @@
 # Updates $USER as owner so sudo is not required on k3s/docker
 # Enables containerd
 # Checks versions to confirm installation
-sudo apt-get -y install git && sudo apt-get -y install policycoreutils 
+sudo apt-get -y install git && sudo apt-get -y install policycoreutils &&
 wait
 # docker install
-sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get update -y && sudo apt-get upgrade -y &&
 wait
-sudo curl -fsSL https://get.docker.com -o get-docker.sh 
+sudo curl -fsSL https://get.docker.com -o get-docker.sh &&
 wait
-sudo bash get-docker.sh
+sudo bash get-docker.sh &&
 wait
 # docker own
-sudo usermod -aG docker $USER && newgrp docker && sudo chown "$USER":"$USER" /home/"$USER"/.docker -R && chmod g+rwx "$HOME/.docker" -R  
+sudo usermod -aG docker $USER && newgrp docker && sudo chown "$USER":"$USER" /home/"$USER"/.docker -R && chmod g+rwx "$HOME/.docker" -R &&
 wait
 # docker startup
-sudo systemctl enable docker.service && sudo systemctl enable containerd.service 
+sudo systemctl enable docker.service && sudo systemctl enable containerd.service &&
 wait
 # k3d install
-sudo wget -O k3d-linux-arm64 https://github.com/rancher/k3d/releases/download/v3.1.5/k3d-linux-arm64 && sudo mv k3d-linux-arm64 /usr/local/bin/k3d && sudo chmod +x /usr/local/bin/k3d 
+sudo wget -O k3d-linux-arm64 https://github.com/rancher/k3d/releases/download/v3.1.5/k3d-linux-arm64 && sudo mv k3d-linux-arm64 /usr/local/bin/k3d && sudo chmod +x /usr/local/bin/k3d &&
 wait
 # k3s install
-sudo curl -sfL https://get.k3s.io | sh 
+sudo curl -sfL https://get.k3s.io | sh &&
 wait
 # k3s own
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 # good practice
-sudo apt-get upgrade -y && apt-get update -y
+sudo apt-get upgrade -y && apt-get update -y &&
 wait
 # Check install verify versions and list nodes
-k3s --version && k3d --version && docker --version && kubectl get nodes 
+k3s --version && k3d --version && docker --version && kubectl get nodes &&
 wait
 # print token
 sudo cat /var/lib/rancher/k3s/server/node-token
