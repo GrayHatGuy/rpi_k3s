a86b3fd096dd2def9c0ef0eab01b7fa50d3d3e98
Jason Russell <grayhatguy@grayhatguy.com>
2023-11-04 16:49:57 -0400

    Update install_controller_1v1.sh

--- bin/install_controller_1v1.sh
+++ bin/install_controller_1v1.sh
@@ -5,21 +5,22 @@
 # Enables containerd
 # Checks versions to confirm installation
 sudo apt-get -y install git && sudo apt-get -y install policycoreutils ; wait
-# root install shell
-sudo -s
 # docker install
-apt-get update -y && apt-get upgrade -y && curl -fsSL https://get.docker.com -o get-docker.sh && bash get-docker.sh
+sudo apt-get update -y && sudo apt-get upgrade -y
+wait
+sudo curl -fsSL https://get.docker.com -o get-docker.sh 
+wait
+sudo bash get-docker.sh
 wait
 # docker own
 sudo usermod -aG docker $USER && newgrp docker && sudo chown "$USER":"$USER" /home/"$USER"/.docker -R && chmod g+rwx "$HOME/.docker" -R  
 wait
 # docker startup
 sudo systemctl enable docker.service && sudo systemctl enable containerd.service ; wait
-exit
 # k3d install
 sudo wget -O k3d-linux-arm64 https://github.com/rancher/k3d/releases/download/v3.1.5/k3d-linux-arm64 && sudo mv k3d-linux-arm64 /usr/local/bin/k3d && sudo chmod +x /usr/local/bin/k3d ; wait
 # k3s install
-curl -sfL https://get.k3s.io | sh ; wait
+sudo curl -sfL https://get.k3s.io | sh ; wait
 # k3s own
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 # good practice
